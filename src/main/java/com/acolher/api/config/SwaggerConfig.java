0067c66443246a40952689e1a228d9b629a541d6
daienelima <daiene.m17@gmail.com>
2019-08-27 00:09:38 -0300

    commit inicial

---
+++ src/main/java/com/acolher/api/config/SwaggerConfig.java
@@ -0,0 +1,45 @@
+package com.acolher.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	
+	//http://localhost:8080/swagger-ui.html
+			@Bean
+		    public Docket productApi() {
+		        return new Docket(DocumentationType.SWAGGER_2)
+		                .select()
+		                .apis(RequestHandlerSelectors.any())
+		                .paths(PathSelectors.ant("/api/**"))
+		                .build()
+		                .apiInfo(this.informacoesApi().build());
+		    }
+		   
+			public ApiInfoBuilder informacoesApi() {
+				ApiInfoBuilder apiInfoBuilder = new ApiInfoBuilder();
+				 
+				apiInfoBuilder.title("Api-Acolher");
+				apiInfoBuilder.description("Backend projeto acolher");
+				apiInfoBuilder.version("1.0");
+				apiInfoBuilder.termsOfServiceUrl("");
+				apiInfoBuilder.contact(this.contato());
+				
+				return apiInfoBuilder;
+			}
+			
+			private Contact contato() {
+				return new Contact(
+						"https://github.com/daienelima/acolher", null, null);
+			}
+}
---
+++ src/test/java/com/acolher/api/AcolherApplicationTests.java
@@ -0,0 +1,16 @@
+package com.acolher.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AcolherApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
